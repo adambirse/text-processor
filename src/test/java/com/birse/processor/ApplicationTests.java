9a91e564650a1f8491300b062e0dd2df1ae8ffdd
adambirse <adambirse@gmail.com>
2019-03-11 21:19:34 +0000

    Fixed unit tests

--- src/test/java/com/birse/processor/ApplicationTests.java
+++ src/test/java/com/birse/processor/ApplicationTests.java
@@ -9,7 +9,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @DirtiesContext
-public class ApplicationTests {
+public class ApplicationTests extends KafkaProducerTest {
 
 	@Test
 	public void contextLoads() {
